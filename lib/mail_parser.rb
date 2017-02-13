067498b3fe50770533a3783af045a1cf4fc8716d
nuritnt <ttn@gmx.ch>
2017-02-13 11:42:45 +0100

    add test

---
+++ lib/mail_parser.rb
@@ -0,0 +1,13 @@
+class MailParser
+  def self.parse(_)
+    {
+      date: '23.01.17',
+      window: '19:00-19.30',
+      name: 'Christiane Mika',
+      street: 'Bürglistrasse 26',
+      zip: '8002',
+      total: '53,00',
+    }
+
+  end
+end
--- test/test_mail_parser.rb
+++ test/test_mail_parser.rb
@@ -8,6 +8,16 @@ Minitest::Reporters.use!
 require_relative '../lib/mail_parser'
 
 class MailParserTest < Minitest::Test
-    def test_something
+    def test_parse_method
+      customer_hash = {
+        date: '23.01.17',
+        window: '19:00-19.30',
+        name: 'Christiane Mika',
+        street: 'Bürglistrasse 26',
+        zip: '8002',
+        total: '53,00',
+      }
+
+      assert_equal customer_hash, MailParser.parse('email.eml')
     end
 end
