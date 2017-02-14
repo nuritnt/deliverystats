8f9747a05d7346bb5975380ae125a38233c5d391
nuritnt <ttn@gmx.ch>
2017-02-14 09:19:48 +0100

    return array with index

--- lib/mail_parser.rb
+++ lib/mail_parser.rb
@@ -4,7 +4,16 @@ class MailParser
   def self.parse(email)
     email_string = Mail.read(email).body.parts.first.raw_source
     email_array = email_string.split("\n")
+    def get_index_of_array(string)
+      kundeninformation = email_array.index("> Kundeninformation\r")
+      date_and_window = kundeninformation -= 1
+      name = [kundeninformation += 1]
+      street = [kundeninformation += 3]
+      zip = [kundeninformation += 4]
+    end
   end
 end
 
 puts MailParser.parse('test/email.eml')
+
+# mail_array[37] will print out the array
